edacadeb4e7a3ef4a00a0b6b1f92ea5364cfdb7f
Alexander Ryzhov <github@ryzhov-al.ru>
2015-12-13 07:25:02 -0500

    Exit when no hash given

--- tracker-add.sh
+++ tracker-add.sh
@@ -4,6 +4,11 @@ torrent_hash=$1
 base_url='http://torrentz.eu'
 pattern='announcelist_[0-9]+'
 
+if [ -z "$1" ] ; then
+    echo "usage: ./tracker-add sh <hash>"
+    exit 1
+fi
+
 announce_list=`curl -s ${base_url}/${torrent_hash} | grep -Eo "${pattern}"`
 
 for tracker in $(curl -s ${base_url}/${announce_list})
